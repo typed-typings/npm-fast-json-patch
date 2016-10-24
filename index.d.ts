be344f8749c44d01cab4494bcdc93218a4b5be91
ppetzold <ppetzold89@gmail.com>
2016-10-24 14:09:26 +0200

    Improve type definition

--- index.d.ts
+++ index.d.ts
@@ -29,17 +29,17 @@ export interface Observer<T> {
  * the removed object (operations that remove things)
  * or just be undefined
  */
-export function apply(tree: any, patches: JsonPatch[], validate?: boolean): Array<any>;
+export function apply(tree: Object, patches: JsonPatch[], validate?: boolean): Array<any>;
 
 /**
  * Observes changes made to an object, which can then be retieved using generate
  */
-export function observe<T>(object: T, callback?: () => void): Observer<T>;
+export function observe<T>(object: T, callback?: () => void): any;
 
 /**
  * Detach an observer from an object
  */
-export function unobserve<T>(object: T, observer: Observer<T>): never;
+export function unobserve<T>(object: T, observer: Observer<T>): void;
 
 /**
  * Generate an array of patches from an observer
@@ -49,14 +49,14 @@ export function generate<T>(observer: Observer<T>): JsonPatch[];
 /**
  * Create an array of patches from the differences in two objects
  */
-export function compare(object1: any, object2: any): JsonPatch[];
+export function compare(object1: Object, object2: Object): JsonPatch[];
 
 /**
  * Ensure a set of patch instructions is valid
  */
-export function validate(sequence: JsonPatch[], tree?: any): JsonPatchError | undefined;
+export function validate(sequence: JsonPatch[], tree?: Object): JsonPatchError | undefined;
 
 /**
  * Validates a single operation. Called from `jsonpatch.validate`. Throws `JsonPatchError` in case of an error
  */
-export function validator(operation: any, index: number, tree?: any, existingPathFragment?: string): never;
+export function validator(operation: JsonPatch, index: number, tree?: Object, existingPathFragment?: string): void;

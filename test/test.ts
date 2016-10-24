3776c72dd1a167c58b63f6b4240403b32d323709
ppetzold <ppetzold89@gmail.com>
2016-10-24 14:01:45 +0200

    Add test

--- test/test.ts
+++ test/test.ts
@@ -1,1 +1,22 @@
-import jsonpatch from 'fast-json-patch';
+import { apply } from 'fast-json-patch';
+
+const obj = {
+    foo: 1,
+    baz: [{
+        qux: 'hello'
+    }]
+};
+
+apply(obj, [{
+    op: 'add',
+    path: '/bar',
+    value: [1, 2, 3, 4]
+}]);
+
+const results = apply(obj, [{
+    op: 'add',
+    path: '/baz/0/foo',
+    value: 'world'
+}]);
+
+console.log(results);
